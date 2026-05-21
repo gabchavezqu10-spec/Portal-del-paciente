b1790ea6068730179924ee91446f1da3ba093949
gabchavezqu10-spec <gabchavezqu10@gmail.com>
2026-05-21 07:35:43 -0500

    Add src/lib/app-params.js

---
+++ src/lib/app-params.js
@@ -0,0 +1,6 @@
+export const appParams = {
+  appId: '69af534df68942d25938ae14',
+  token: import.meta.env.VITE_BASE44_APP_TOKEN || '',
+  functionsVersion: 'v1',
+  appBaseUrl: import.meta.env.VITE_BASE44_APP_BASE_URL || 'https://base44.app',
+};
